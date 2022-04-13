f4e89a8d6397783b02395f725265f673fa3c84db
Sergey <97399560+GSilver27@users.noreply.github.com>
2022-04-13 20:07:07 +0300

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,35 @@
+    <footer class="wrapper">
+        <div class="footer__line"></div>
+        <div class="footer__outer d-flex justify-content-between align-items-center">
+            <div class="footer__inner d-flex justify-content-start align-items-center">
+                <div class="footer__item mt-4">
+                    <div class="footer__item">
+                        <a href="tel:8 495 118 39 34" class="phone">8 495 118 39 34</a>
+                    </div>
+                    <p class="footer__text-info">
+                        <a href="../oferta.pdf" target="_blank">
+                            *отправляя формы на данном сайте, вы даете согласие на
+                            обработку персональных данных в соответствии с 152-ФЗ
+                        </a>
+                    </p>
+                </div>
+
+                <div class="footer__item footer__politics">
+                    <a href="../policy.html" target="_blank">Политика конфиденциальности</a>
+                </div>
+            </div>
+
+            <div class="footer__inner">
+                <a href="/" class="logo">MYFORCE</a>
+                <p class="logo__text">Экосистема для вашего бизнеса</p>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="../js/jquery-3.5.1.min.js"></script>
+    <script src="../js/jquery.maskedinput.js"></script>
+    <script src="../js/scripts.js"></script>
+
+</body>
+</html>
